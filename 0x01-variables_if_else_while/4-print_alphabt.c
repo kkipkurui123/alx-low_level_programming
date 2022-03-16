f40066e1eb0f0c30a805085c3ccce616230bdba0
kkipkurui123 <kkipkurui123@gmail.com>
2022-03-16 15:40:27 -0700

    my 5th task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char alphabet;
+
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+	if(alphabet == 'e')
+	continue;
+
+	else if(alphabet == 'q')
+	continue;
+
+	putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
