a8798dbf65850bf63e7bed66d3b9a400d9d2ba85
kkipkurui123 <kkipkurui123@gmail.com>
2022-03-18 15:39:26 -0700

    my 1st task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c);
+
+int main(void)
+{
+	int c;
+
+	if ((c = 'C'))
+	{
+	return (1);
+	}
+
+	else (c = 'c');
+	{
+	return (0);
+	}
+}
